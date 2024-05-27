b18af525361ba7ffead212ed65c63a02976217bf
Creszra <josephrmagabilin@gmail.com>
2024-05-28 03:22:02 +0800

    Added Delete Post function

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Posts } = require('../models')
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 
 router.get("/", async (req, res) => {
@@ -15,10 +16,22 @@ router.get('/byId/:id', async (req, res) => {
 });
 
 
-router.post("/", async (req, res) => {
-    const post = req.body
-    await Posts.create(post);   
+router.post("/", validateToken, async (req, res) => {
+    const post = req.body;
+    post.username = req.user.username;
+    await Posts.create(post);
     res.json(post);
-});
+  });
+
+router.delete("/:postId", validateToken, async (req, res) => {
+    const postId = req.params.postId;
+    await Posts.destroy({
+      where: {
+        id: postId,
+      },
+    });
+  
+    res.json("DELETED SUCCESSFULLY");
+  });
 
 module.exports = router
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import axios from "axios"; 
 import { useEffect, useState } from "react"; 
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from "../helpers/AuthContext";
 
 function Home() {
     const [listofPosts, setlistofPosts] = useState([]);
     let navigate = useNavigate();
+    const { authState } = useContext(AuthContext);
 
 
     useEffect(() => {
+
+      if (!localStorage.getItem("accessToken")) {
+        navigate("/login");
+      } else {
       axios.get("http://localhost:3001/posts").then((response) => {
         setlistofPosts(response.data);
       });
+    }
     }, []);
-
+  
   return (
     <div>
       {listofPosts.map((value, key) =>  {
