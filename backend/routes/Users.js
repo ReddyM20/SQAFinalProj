683310f29a387f60fb8ed21884d1a2f710639af8
Creszra <josephrmagabilin@gmail.com>
2024-05-29 20:13:08 +0800

    Update Users.js

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -21,18 +21,26 @@ router.post("/login", async (req, res) => {
 
   const user = await Users.findOne({ where: { username: username } });
 
-  if (!user) res.json({ error: "User Doesn't Exist" });
+  if (!user) {
+    res.json({ error: "User Doesn't Exist" });
+  } else {
 
   bcrypt.compare(password, user.password).then(async (match) => {
-    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+    if (!match){
+
+    res.json({ error: "Wrong Username And Password Combination" });
+
+    }else {
 
     const accessToken = sign(
       { username: user.username, id: user.id },
       "importantsecret"
     );
     res.json({ token: accessToken, username: username, id: user.id });
+    }
   });
-});
+}
+}); 
 
 router.get("/auth", validateToken, (req, res) => {
   res.json(req.user);
